e60dfbd98e572fe380522bd7b4ffc72b50c01356
klfeltrin <kelvin.delucca@gmail.com>
2021-05-12 21:45:22 -0300

    Logica inicial das regras de despacho

---
+++ dispatching_rules/dispatching_rules.py
@@ -0,0 +1,28 @@
+import random
+
+def dispatch_rule_1(t_cur, tard_job, UC_job):
+    if not tard_job:
+        j = min()
+    else:
+        j = max()
+    
+    j = OP + 1
+    M = min()
+    # assign Oij on Mk
+
+def dispatch_rule_2(t_cur, tard_job, UC_job):
+    if not tard_job:
+        j = min()
+    else:
+        j = max()
+    
+    j = OP + 1
+    M = min()
+    # assign Oij on Mk
+
+def dispatch_rule_3(t_cur, UC_job, ji, j):
+    if random.uniform(0, 1) < 0.5:
+        M = min()
+    else:
+        M = min()
+    # assign Oij on Mk
