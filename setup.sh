7c9270f4fa4af2279a8438fc92851374c4108aa2
klfeltrin <kelvin.delucca@gmail.com>
2021-05-19 20:52:19 -0300

    Alteracao no setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-mkdir -p ~/.streamlit/
-
+ mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"kelvin.feltrin@edu.unifil.br\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
